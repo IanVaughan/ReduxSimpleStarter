bbf37a9522fdae12060fbf0da6b89eb312476f15
Ian Vaughan <github@ianvaughan.co.uk>
2016-05-18 22:24:38 +0100

    5.57

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = 'b662d0f37db286e0cc014da5b5af5e01';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export cosnt FETCH_WEATHER = 'FETCH_WEATHER'
+export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},uk`
